e4017c38e37f32bba4e6db3a47cae691250a5659
sktt1-win <gkswh4860@naver.com>
2020-09-09 16:51:28 +0900

    player 생성

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class asd : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
