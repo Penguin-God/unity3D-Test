f3f40ec6613572c2ca7c4f3a8454677c274da2f3
sktt1-win <gkswh4860@naver.com>
2020-09-10 17:41:55 +0900

    player jump 구현

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,13 +8,17 @@ public class Player : MonoBehaviour
 
     private float hAxis;
     private float xAxis;
-    bool Walk;
+    bool WalkKey;
+    bool JumpKey;
+    bool isJump;
 
     Vector3 MoveVec;
     Animator animator;
+    new Rigidbody rigidbody;
 
     private void Awake()
     {
+        rigidbody = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>(); // Animator 컴포넌트가 Player의 자식에게 있기 때문에 GetComponentInChildren<>을 사용
     }
 
@@ -23,29 +27,52 @@ public class Player : MonoBehaviour
         GetInput();
         PlayerMove();
         PlayerTurn();
+        Jump();
     }
     
     void GetInput()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
         hAxis = Input.GetAxisRaw("Vertical");
-        Walk = Input.GetButton("Walk");
+        WalkKey = Input.GetButton("Walk");
+        JumpKey = Input.GetButtonDown("Jump");
     }
 
     void PlayerMove()
     {
         // 이동
         MoveVec = new Vector3(xAxis, 0, hAxis).normalized; // normalized : 방향 값이 1로 보정된 백터(저걸 안하면 대각선 이동 시 평소보다 더 빠르게 이동함)
-        transform.position += MoveVec * speed * (Walk ? 0.3f : 1f) * Time.deltaTime; // transform이동은 Time.dalraTime을 넣어줘야 함
+        transform.position += MoveVec * speed * (WalkKey ? 0.3f : 1f) * Time.deltaTime; // transform이동은 Time.dalraTime을 넣어줘야 함
 
         // 애니메이션
         animator.SetBool("IsRun", MoveVec != Vector3.zero); // Vector3.zero = Vector3(0, 0, 0); 즉 모든 Vector값이 0이 아니면 "IsRun"은 true
-        animator.SetBool("IsWalk", Walk);
+        animator.SetBool("IsWalk", WalkKey);
     }
 
     void PlayerTurn() // 회전
     {
         // player가 나아가는 방향을 바라보는 코드
         transform.LookAt(transform.position + MoveVec); // LookAt() : 지정된 백터를 향해서 회전시켜주는 함수 
+    }
+
+    void Jump()
+    {
+        if (JumpKey && !isJump)
+        {
+            // .AddForce(힘, 유형) : Rigidbody에 힘을 추가한다   Impulse : 질량을 사용하여 리지드 바디에 순간적인 힘 임펄스를 추가
+            rigidbody.AddForce(Vector3.up * 13, ForceMode.Impulse); // 편집 -> 프로젝트 세팅-> 물리에가면 중력값 조정 가능
+            isJump = true;
+            animator.SetBool("IsJump", isJump);
+            animator.SetTrigger("DoJump"); // Trigger : 트리거가 호출되는 순간에 한 번 켜지고, 트리거 조건이 있는 트랜지션을 통과하면 자동으로 꺼지기 때문에 별다른 세부 조건이 없다.
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("바닥"))
+        {
+            isJump = false;
+            animator.SetBool("IsJump", isJump);
+        }
     }
 }
