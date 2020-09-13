aa7e2612b323b90e9f938d80b3d141f810f96f34
Penguin-God <gkswh4860@naver.com>
2020-09-13 15:19:06 +0900

    아이템 물리충돌 제거

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -9,8 +9,26 @@ public class Item : MonoBehaviour
     public Type type; // enum은 하나의 타입이므로 담을 변수가 따로 필요
     public int value;
 
+    new Rigidbody rigidbody;
+    SphereCollider sphereCollider;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
     private void Update()
     {
         transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "바닥")
+        {
+            rigidbody.isKinematic = true;
+            sphereCollider.enabled = false; // enabled : 활성화된 콜라이더는 다른 콜라이더와 충돌하고 비활성화된 콜라이더는 충돌하지 않음
+        }
     }
 }
