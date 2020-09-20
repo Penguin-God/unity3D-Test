6197ea585477ea5e39eb09b9a59155ca20a086c4
Penguin-God <gkswh4860@naver.com>
2020-09-20 19:48:52 +0900

    근접공격범위 설정

---
+++ Assets/Script/Weapons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public enum Type { Melee, Range};
+    public Type type;
+    public int Damge;
+    public float 공속;
+    public BoxCollider2D AttackRange;
+    public TrailRenderer trailEffect;
+
+}
