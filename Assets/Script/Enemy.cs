0c8bf0a634c2a3edb7be1c3700a70d9f6a4fe573
Penguin-God <gkswh4860@naver.com>
2020-10-02 22:01:27 +0900

    피격 구현

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -9,11 +9,13 @@ public class Enemy : MonoBehaviour
 
     BoxCollider box;
     Rigidbody rigid;
+    Material Mat;
 
     private void Awake()
     {
         box = GetComponent<BoxCollider>();
         rigid = GetComponent<Rigidbody>();
+        Mat = GetComponent<MeshRenderer>().material; // Material은 MeshRenderer에서 가져와야 됨
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,11 +24,16 @@ public class Enemy : MonoBehaviour
         {
             Weapons weapon = other.GetComponent<Weapons>();
             CurrentHp -= weapon.Damage;
+            StartCoroutine(OnDamage(DamageVec));
         }
         else if(other.tag == "총알")
         {
             총알 총알 = other.GetComponent<총알>();
             CurrentHp -= 총알.Damage;
+        DamageVec = DamageVec.normalized; // normalized : 백터의 방향은 같지만 크기는 1.0을 반환함
+        DamageVec += Vector3.up * 0.5f; // Vector3.up : Vector(0, 1, 0) 
+            rigid.AddForce(DamageVec * Random.Range(1f, 2f), ForceMode.Impulse);
+            rigid.AddForce(DamageVec * Random.Range(7f, 15f), ForceMode.Impulse);
         }
     }
 }
