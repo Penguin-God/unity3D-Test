39a8e66a8282b8369c5b278a9cc49b8463c3a335
Penguin-God <gkswh4860@naver.com>
2020-10-25 14:13:11 +0900

    수류탄 RayCast Draw 추가

--- Assets/Script/GrenadeBoom.cs
+++ Assets/Script/GrenadeBoom.cs
@@ -21,10 +21,16 @@ public class GrenadeBoom : MonoBehaviour
         MashObj.SetActive(false);
         EffectObj.SetActive(true);
 
-        RaycastHit[] rayHits = Physics.SphereCastAll(this.transform.position, 15, Vector3.up, 0f, LayerMask.GetMask("Enemy")); // 구 모양의 Ray를 쏨
+        RaycastHit[] rayHits = Physics.SphereCastAll(this.transform.position, 10, Vector3.up, 0f, LayerMask.GetMask("Enemy")); // 구 모양의 Ray를 쏨
         foreach(RaycastHit hitObjs in rayHits)
         {
             hitObjs.transform.GetComponent<Enemy>().HitByGrenade(this.transform.position);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, 10);
     }
 }
