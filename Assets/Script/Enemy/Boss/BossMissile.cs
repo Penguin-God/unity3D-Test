3459080926a3ac0c4bedae608ad5656f051a4426
Penguin-God <gkswh4860@naver.com>
2021-01-15 16:51:08 +0900

    create Boss Messile

---
+++ Assets/Script/Enemy/Boss/BossMissile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossMissile : 총알삭제
+{
+    public Transform target;
+    NavMeshAgent nav;
+
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(target.position);
+    }
+}
