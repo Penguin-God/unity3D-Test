23fadb9b0a1fc922bcfa8419700c9a7e92f527cd
Penguin-God <gkswh4860@naver.com>
2021-03-02 20:30:55 +0900

    Random Enemy Respawn

--- Assets/Script/Test/EntraStage.cs
+++ Assets/Script/Test/EntraStage.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class EntraStage : MonoBehaviour
 {
-    
+    public Transform[] RespawnEnemyPositions;
+    public GameObject[] RespawnEnemys;
+
+    void EnemyRespawn()
+    {
+        for(int i = 0; i < RespawnEnemyPositions.Length; i++)
+        {
+            int random = Random.Range(0, RespawnEnemys.Length);
+            Instantiate(RespawnEnemys[random], RespawnEnemyPositions[i].position, RespawnEnemyPositions[i].rotation);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            EnemyRespawn();
+        }
+    }
 }
