472c2356d4aa12a79e03be1605d22da80b798037
Penguin-God <gkswh4860@naver.com>
2021-05-01 15:15:33 +0900

    베르 nav 2강

---
+++ Assets/Practice-NavMesh/FallTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTrigger : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rigid = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rigid.useGravity = true;
+    }
+}
---
+++ Assets/Practice-NavMesh/Moveable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moveable : MonoBehaviour
+{
+    NavMeshAgent nav;
+    [SerializeField]
+    Transform target = null;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            nav.SetDestination(target.position);
+        }
+    }
+}
