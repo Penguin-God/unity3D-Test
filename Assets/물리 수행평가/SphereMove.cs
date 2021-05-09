0cccd3b9eac5b5c7f9d7be7a33030e3cf91a14eb
Penguin-God <gkswh4860@naver.com>
2021-05-09 18:44:02 +0900

    물리 수행평가

---
+++ Assets/물리 수행평가/SphereMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMove : MonoBehaviour
+{
+    Vector3 toCubeDirection;
+    public GameObject Cube;
+    Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        toCubeDirection = (Cube.transform.position - transform.position).normalized;
+        Vector3 LookDirection = (transform.position - Cube.transform.position).normalized;
+        rigid.velocity = toCubeDirection + Vector3.forward;
+        transform.LookAt(Vector3.Cross(Vector3.right, LookDirection));
+        Debug.Log(Vector3.Cross(Vector3.right, LookDirection));
+    }
+}
